c14d948207b25e4947ea8c4daaee3a581896fc7a
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-09 00:32:41 +0100

    serve static assets 8

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+app.use('/public', express.static(__dirname + '/public'));
 app.get("/",function(req, res) {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
     }
   );
-app.use('/public', express.static(__dirname + '/public'));
 
 
 
