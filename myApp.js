8dea5537da3d74bc03ad236029b758a393302b2f
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-09 00:29:32 +0100

    serve static assets attempt 6

--- myApp.js
+++ myApp.js
@@ -5,8 +5,7 @@ app.get("/",function(req, res) {
     res.sendFile(absolutePath = __dirname + '/views/index.html');
     }
   );
-app.use('/public', function(req, res) {
-    express.static();
+app.use('/public', express.static(__dirname + '/public'));
     }
 );
 
