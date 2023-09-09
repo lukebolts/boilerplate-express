3ca80ee75c3085ebfaa88abaf50dde398cbcfc6b
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-09 10:38:04 +0100

    serve json

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
-app.get("/",function(req, res) {
-    res.sendFile(absolutePath = __dirname + '/views/index.html');
+app.get("/json",function,(req, res) {
+    res.json("message": "Hello json");
     }
   );
 
