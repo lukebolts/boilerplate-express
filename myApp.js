c19393ae70e7ce043e06da977b1f52f9de41d716
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-09 10:41:08 +0100

    serve json

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
-app.get("/json",function,(req, res) {
-    res.json("message": "Hello json");
+app.get("/json",function(req, res) {
+    res.json({"message" : "Hello json"});
     }
   );
 
