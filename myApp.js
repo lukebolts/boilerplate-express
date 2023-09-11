962ed70118520a4851ef91424b033abfd1d7d0c5
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 09:21:48 +0100

    use the .env file 5

--- myApp.js
+++ myApp.js
@@ -1,15 +1,14 @@
 let express = require('express');
 let app = express();
-let response = "";
-if (process.env.MESSAGE_STYLE === "uppercase"){
-    response = "Hello json".toupperCase();
-    }
-    else {
-    response = "Hello json"
-    }
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
 app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        response = "Hello json".toupperCase();
+        }
+        else {
+        response = "Hello json"
+        }
     res.json({"message" : response});
     }
   );
