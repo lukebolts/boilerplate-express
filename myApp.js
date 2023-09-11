06738666ed833d41bbcfb2b437e38361778d4904
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 09:32:07 +0100

    debugging again

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
 app.get("/json", function(req, res) {
     console.log(process.env.MESSAGE_STYLE)
-    let response = "Hello json".toUpperCase;
+    let response = "Hello json".toUpperCase();
     console.log(response)
     if (process.env.MESSAGE_STYLE === "uppercase"){
         response = "Hello json".toUpperCase();
