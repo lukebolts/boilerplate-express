09fda0bce8081e2765ef9a3c65a482f4e245afee
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 09:36:31 +0100

    add require dotenv

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
 app.get("/json", function(req, res) {
     console.log(process.env.MESSAGE_STYLE)
-    let response = "Hello json".toUpperCase();
-    console.log(response)
+    let response = "";
     if (process.env.MESSAGE_STYLE === "uppercase"){
         response = "Hello json".toUpperCase();
         }
