4e9df929a032905d442de56b4550b406b28a6a38
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 18:30:34 +0100

    middleware logger 2

--- myApp.js
+++ myApp.js
@@ -2,18 +2,24 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 app.use('/public', express.static(__dirname + '/public'));
-app.get("/json", function(req, res) {
+app.use("/", function(req, res) {
     let method = req.method;
     let path = req.path;
     let ip = req.ip;
-    // if (process.env.MESSAGE_STYLE === "uppercase"){
-    //     response = "Hello json".toUpperCase();
-    //     }
-    //     else {
-    //     response = "Hello json"
-    //     }
-    res.json({"middlewareLogger" : method + " " + path + " - " + ip});
+    console.log(method + " " + path + " - " + ip);
     next();
+    }
+)
+app.get("/json", function(req, res) {
+    let method = req.method;
+
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        response = "Hello json".toUpperCase();
+        }
+        else {
+        response = "Hello json"
+        }
+    res.json(response);
     }
   );
 
