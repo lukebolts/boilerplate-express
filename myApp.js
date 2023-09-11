cf1b9b307c1372a172c606fc4cbbb0f1d2340add
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 19:19:57 +0100

    word echo

--- myApp.js
+++ myApp.js
@@ -31,6 +31,12 @@ app.get("/now", function(req, res, next){
     }
   )
 
+app.get("/:word/echo", function(req, res){
+    let word = req.params.word
+    },
+    res.json({"echo": word})
+)
+
 
 
 
