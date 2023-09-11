6befae69368e38dda6bfef36fa622dd921c7a0da
lukebolts <53878880+lukebolts@users.noreply.github.com>
2023-09-11 09:25:57 +0100

    use .env file debugging 2

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ console.log("Hello World");
 app.use('/public', express.static(__dirname + '/public'));
 app.get("/json", function(req, res) {
     console.log(process.env.MESSAGE_STYLE)
-    console.log("Hello json".toupperCase())
+    let response = "";
+    console.log(response.toupperCase();)
     if (process.env.MESSAGE_STYLE === "uppercase"){
         response = "Hello json".toupperCase();
         }
